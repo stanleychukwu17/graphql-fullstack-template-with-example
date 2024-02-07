1a6c3cf82457c699fd1682bd43107437851961f5
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-02-07 10:12:12 +0100

    added the configuration for jest and also running the first test case

---
+++ client/__tests__/page.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '@/app/page'
+import { describe } from 'node:test'
+
+const useMock = jest.mock().fn()
+
+describe("Page test", () => {
+    it("should show home", () => {
+    })
+})
