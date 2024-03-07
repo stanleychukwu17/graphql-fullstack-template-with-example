c8701068bce038382754bf269e64d7f8778a3da0
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-03-07 18:41:57 +0100

    re-factored the code to allow for better testing of the code

--- client/app/components/Header/Header.tsx
+++ client/app/components/Header/Header.tsx
@@ -13,22 +13,45 @@ import LoggedOutCard from "./LoggedOutCard";
 
 //--START-- checks to see if there are any stored information about the user in the user's localStorage space
 let userDts: userDetailsType = {loggedIn: 'no'}
-try {
-    const cached_user_dts  = window.localStorage.getItem('userDts') // the user details stored to the localStorage whenever a user logs in
+export function update_the_userDetails_information (cached_user_details: string|null|object) {
+    if (cached_user_details) {
+        let user_dtsParsed;
 
-    if (cached_user_dts) {
-        // const cached_user_parsed = JSON.parse(cached_user_dts) as unknown as userDetailsType
-        const cached_user_parsed = JSON.parse(cached_user_dts)
+        if (typeof cached_user_details === 'string') {
+            user_dtsParsed = JSON.parse(cached_user_details as string)
+        } else if (typeof cached_user_details === 'object') {
+            user_dtsParsed = cached_user_details
+        }
     
         userDts.loggedIn = 'yes'
-        userDts = {...userDts, ...cached_user_parsed}
+        userDts = {...userDts, ...user_dtsParsed}
     }
-} catch (err) {}
+}
+
+const cached_user_dts = window.localStorage.getItem('userDts') // the user details stored to the localStorage whenever a user logs in
+update_the_userDetails_information(cached_user_dts);
 //--END--
 
 //--START-- validates the accessToken and Refresh token every 24_hour
-function run_access_token_health_check () {
-    axios.post(`${backEndPort}/healthCheck/accessToken`, userDts, {headers: {'Content-Type': 'application/json'}})
+export function check_if_we_can_run_the_access_token_health_check (uDts: userDetailsType) {
+    const last_24hr_check = localStorage.getItem('last_24hr_check')
+
+    if (last_24hr_check) {
+        const storedDate = new Date(last_24hr_check).getTime() // .getTime() returns the number of milliseconds
+        const currentDate = new Date().getTime() // .getTime() returns the number of milliseconds
+        const hourDiff = (currentDate - storedDate) / (1000 * 60 * 60); // converts the difference to hours.. since i want to know if the last check has been older than an 24hours
+    
+        if (hourDiff >= 24 && uDts.loggedIn === 'yes') {
+            run_access_token_health_check(uDts)
+        }
+    } else {
+        const current_time = new Date()
+        localStorage.setItem('last_24hr_check', `${current_time}`)
+    }
+}
+
+export function run_access_token_health_check (uDts: userDetailsType) {
+    axios.post(`${backEndPort}/healthCheck/accessToken`, uDts, {headers: {'Content-Type': 'application/json'}})
     .then(re => {
         // update the lastTime checked to be the current time
         localStorage.setItem('last_24hr_check', `${new Date()}`)
@@ -51,21 +74,7 @@ function run_access_token_health_check () {
     })
 }
 
-try {
-    const last_24hr_check = localStorage.getItem('last_24hr_check')
-    if (last_24hr_check) {
-        const storedDate = new Date(last_24hr_check).getTime() // .getTime() returns the number of milliseconds
-        const currentDate = new Date().getTime() // .getTime() returns the number of milliseconds
-        const hourDiff = (currentDate - storedDate) / (1000 * 60 * 60); // converts the difference to hours.. since i want to know if the last check has been older than an 24hours
-    
-        if (hourDiff >= 24 && userDts.loggedIn === 'yes') {
-            run_access_token_health_check()
-        }
-    } else {
-        const current_time = new Date()
-        localStorage.setItem('last_24hr_check', `${current_time}`)
-    }
-} catch (err) {}
+check_if_we_can_run_the_access_token_health_check(userDts)
 //--END--
 
 
@@ -73,6 +82,7 @@ export default function Header() {
     const userInfo = useAppSelector(state => state.user)
     const reduxDispatch = useAppDispatch()
     const route = useRouter()
+
 
     // console.log(userDts, '\n', userInfo)
     useLayoutEffect(() => {
@@ -85,9 +95,8 @@ export default function Header() {
         }
     }, [route, reduxDispatch, userInfo.must_logged_in_to_view_this_page, userInfo.loggedIn])
 
-
     return (
-        <header className="flex justify-between items-center py-5 px-5 bg-[#e9f2ff]">
+        <header className="flex justify-between items-center py-5 px-5 bg-[#e9f2ff]" data-testid="site header">
             <div className="text-2xl font-bold">
                 <Link href="/">NEXT.</Link>
             </div>
