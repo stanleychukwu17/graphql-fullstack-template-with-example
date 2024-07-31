cfbf9e3c95e797d5c753bf046b5386f489adbae1
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-07-31 14:55:37 +0100

    removed loadash function

--- server/src/controllers/random.controller.ts
+++ server/src/controllers/random.controller.ts
@@ -1,5 +1,4 @@
 // import { errorLogger, log } from '../logger/';
-import { pick } from 'lodash'
 import { pool } from '../db'
 import {show_bad_message, show_good_message} from '../functions/utils'
 
