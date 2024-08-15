9d929f5e28584210f84ecc5d3bda83d1d810df4f
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-15 13:08:53 +0100

    renamed the directory to server-node

--- server/src/types/users.d.ts
+++
@@ -1,9 +0,0 @@
-export type userRegisterInfo = {
-    name: string;
-    username: string;
-    email: string;
-    password: string;
-    gender: 'male'|'female';
-}
-
-export type userLoginInfo = Pick<userRegisterInfo, "username" | "password">
