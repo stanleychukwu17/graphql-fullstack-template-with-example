8d5e5c34b4d8f6ecdf93aad2b4ae9f74a0d3fc72
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-16 13:43:28 +0100

    now listening to request using fiber v2

--- server-golang/main.go
+++ server-golang/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Access environment variables
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		log.Fatalf("PORT environment variable is required but not set")
+	}
+
+	// Create new Fiber instance
+	app := fiber.New()
+
+	// Create GET route on path "/"
+	app.Get("/healthCheck", func(context *fiber.Ctx) error {
+		return context.SendString("Hello, World!")
+	})
+
+	// Print message to console
+	fmt.Printf(`🚀 Server running on %s, see http://localhost:%s & for healthCheck see http://localhost:%s/healthCheck`, port, port, port)
+
+	// Start server
+	app.Listen(fmt.Sprintf(":%s", port))
+
 }
