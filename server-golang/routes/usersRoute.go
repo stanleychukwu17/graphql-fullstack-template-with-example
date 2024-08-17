b1cacb25430af52cd9fee7f312e523b25d035539
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-17 18:40:33 +0100

    cleaned up the usersRoute to only handle routing

--- server-golang/routes/usersRoute.go
+++ server-golang/routes/usersRoute.go
@@ -1,45 +1,33 @@
 package routes
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/models"
+	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/controllers"
 	"gorm.io/gorm"
 )
 
+// UsersRoutes represents the routes related to user operations in the application.
+// It holds a reference to the database connection used for user-related queries.
 type UsersRoutes struct {
+	// DB is a pointer to a Gorm DB instance used for interacting with the database.
 	DB *gorm.DB
 }
 
+// SetUpRoutes configures the routes related to user operations for the given Fiber application.
+// It sets up the routes under the "/users" path and binds them to the appropriate handler methods.
 func (u *UsersRoutes) SetUpRoutes(app *fiber.App) {
+	// Create a new route group for "/users".
 	api := app.Group("/users")
 
-	api.Get("/", u.GetAllUsers)
+	// initialize the users controller
+	uControl := &controllers.UsersController{DB: u.DB}
+
+	api.Get("/", u.GetAllUsers)                      // Handles GET requests to "/users" by invoking the GetAllUsers method.
+	api.Post("/registerUser", uControl.RegisterUser) // Handles POST requests to "/users/registerNewUser" by invoking the RegisterUser method.
 }
 
+// GetAllUsers gets all the registered users
 func (u *UsersRoutes) GetAllUsers(ctx *fiber.Ctx) error {
-	ctx.Status(fiber.StatusOK).JSON(fiber.Map{"msg": "okay", "cause": "Hello, World!"})
-	return nil
-}
-
-// route to register a new user
-func (u *UsersRoutes) RegisterNewUser(ctx *fiber.Ctx) error {
-	user := models.User{}
-
-	// Parse the request body & bind it to the book struct
-	// the context.BodyParser(&book), is parsing the request body from json to go struct.. Fiber does this internally, be default, Golang does not understand json
-	if err := ctx.BodyParser(&user); err != nil {
-		// Log the error and respond with a 400 Bad Request
-		log.Println("Error parsing request body:", err)
-
-		return ctx.Status(http.StatusUnprocessableEntity).JSON(
-			map[string]interface{}{
-				"message": "Invalid request body",
-			},
-		)
-	}
-
+	ctx.Status(fiber.StatusOK).JSON(fiber.Map{"msg": "okay", "cause": "Users route says:Hello, World!"})
 	return nil
 }
