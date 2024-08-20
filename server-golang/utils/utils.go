7f6ecdee5781559a3c2172b1e424d1dda1696242
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-20 10:46:56 +0100

    added 2 new functions: Generate_fake_id and verify_if_fields_are_empty

--- server-golang/utils/utils.go
+++ server-golang/utils/utils.go
@@ -1,13 +1,30 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
 
-// export const generate_fake_id = (id:number) => {
-//     // Generate a random number between 100 and 999 (inclusive of 100, exclusive of 999)
-//     const randomNumber = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
-//     const new_fake_id = `${id}${randomNumber}`
-//     return Number(new_fake_id);
-// }
+	"golang.org/x/crypto/bcrypt"
+)
+
+type FieldRequirement struct {
+	Key    string `json:"key"`
+	Length int    `json:"length"`
+	Msg    string `json:"msg"`
+}
+
+func Generate_fake_id(id int) int {
+	// Create a new random source
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Generate a random number between 100 and 999
+	randomNumber := r.Intn(900) + 100
+
+	new_fake_id, _ := strconv.Atoi(fmt.Sprintf("%v%v", id, randomNumber))
+	return new_fake_id
+}
 
 // ShowBadMessage generates a standardized error message map.
 func Show_bad_message(cause string) map[string]string {
@@ -70,3 +87,17 @@ func main() {
     }
 }
 */
+
+func Check_if_required_fields_are_present(list []FieldRequirement) (bool, string) {
+	found_error, error_msg := false, ""
+
+	for _, field := range list {
+		if len(field.Key) <= field.Length {
+			found_error = true
+			error_msg = field.Msg
+			break
+		}
+	}
+
+	return found_error, error_msg
+}
