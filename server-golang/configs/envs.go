5cb4b8e305843ebf73a912c04c1b69d493531bf7
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-22 10:09:32 +0100

    refactored, so it can be tested

--- server-golang/configs/envs.go
+++ server-golang/configs/envs.go
@@ -8,29 +8,43 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Config struct {
-	PUBLIC_HOST string
-	PORT        string
-	JWT_SECRET  string
-	JWT_TIME_1  int
-	JWT_TIME_2  int
+type EnvLoader interface {
+	Load() error
 }
 
-var Envs = initConfig()
+type realEnvLoader struct{}
 
-func initConfig() Config {
-	err := godotenv.Load()
+func (realEnvLoader) Load() error {
+	if os.Getenv("ENV") == "test" {
+		return godotenv.Load("../.env.test")
+	} else {
+		return godotenv.Load()
+	}
+}
+
+type Config struct {
+	ENV        string
+	PORT       string
+	JWT_SECRET string
+	JWT_TIME_1 int
+	JWT_TIME_2 int
+}
+
+var Envs, _ = initConfig(realEnvLoader{})
+
+func initConfig(loader EnvLoader) (Config, error) {
+	err := loader.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		return Config{}, err
 	}
 
 	return Config{
-		PUBLIC_HOST: getEnv("PUBLIC_HOST", "http://localhost"),
-		PORT:        getEnv("PORT", "4000"),
-		JWT_SECRET:  getEnv("JWT_SECRET", "not-a-secret-any-more-is-it?"),
-		JWT_TIME_1:  getEnvAsInt("JWT_TIME_1", 7),
-		JWT_TIME_2:  getEnvAsInt("JWT_TIME_2", 365),
-	}
+		ENV:        getEnv("ENV", "should-be-fixed"),
+		PORT:       getEnv("PORT", "4000"),
+		JWT_SECRET: getEnv("JWT_SECRET", "not-a-secret-any-more-is-it?"),
+		JWT_TIME_1: getEnvAsInt("JWT_TIME_1", 7),
+		JWT_TIME_2: getEnvAsInt("JWT_TIME_2", 365),
+	}, nil
 }
 
 // Gets the env by key or fallbacks
