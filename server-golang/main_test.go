7ce1bbe80829115b2db6a6c930aefc90b140519d
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-22 10:12:09 +0100

    cleaned up the test

--- server-golang/main_test.go
+++ server-golang/main_test.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,17 +8,13 @@ import (
 
 func TestSetup(t *testing.T) {
 	// Set environment variable for testing
-	os.Setenv("PORT", "4000")
+	// os.Setenv("PORT", "4000")
 
+	// call setup function
 	app, db, err := setup()
 
-	fmt.Printf("%+v \n", reflect.TypeOf(app))
-
+	// check if setup was successful
 	require.NoError(t, err)
 	require.NotNil(t, app)
 	require.NotNil(t, db)
-
-	// Optionally, you can check more details about the app or db
-	// For example, if you have specific methods or properties
-	// assert.Equal(t, expected, actual)
 }
