f92da0030eab76fa604e1cb82cdd9a894fc33c44
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-22 20:29:28 +0100

    refactored for easier testing

--- server-golang/database/dbConnect.go
+++ server-golang/database/dbConnect.go
@@ -1,14 +1,103 @@
 package database
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+	"time"
 
-	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/utils"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"github.com/docker/go-connections/nat"
+	testContainers "github.com/testcontainers/testcontainers-go"
+	mysqlContainer "github.com/testcontainers/testcontainers-go/modules/mysql"
+	postgresContainer "github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
+
+func SetupTestDB(which_db string) (string, testContainers.Container, error) {
+	var db_container testContainers.Container
+	var err error
+
+	ctx := context.Background()
+
+	fmt.Printf("about to start %v testing container... for %v environment  \n", which_db, os.Getenv("ENV"))
+
+	if which_db == "postgres" {
+		db_user := "postgres"
+		db_password := "password"
+		db_port := os.Getenv("POSTGRES_DB_PORT")
+		db_name := os.Getenv("DB_NAME")
+		db_timezone := os.Getenv("POSTGRES_DB_TIMEZONE")
+
+		db_container, err = postgresContainer.Run(ctx,
+			"postgres:16.3-alpine3.20",
+			postgresContainer.WithDatabase(db_name),
+			postgresContainer.WithUsername(db_user),
+			postgresContainer.WithPassword(db_password),
+			testContainers.WithWaitStrategy(
+				wait.ForLog("database system is ready to accept connections").
+					WithOccurrence(2).
+					WithStartupTimeout(5*time.Second)),
+		)
+		if err != nil {
+			log.Fatalf("failed to start container: %s", err)
+		}
+
+		fmt.Printf("%v Container started \n", which_db)
+		// formats the connection string
+		dsn := fmt.Sprintf("host=localhost user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=%v", db_user, db_password, db_name, db_port, db_timezone)
+
+		// Clean up the container
+		defer func() {
+			if err := db_container.Terminate(ctx); err != nil {
+				log.Fatalf("failed to terminate container: %s", err)
+			} else {
+				fmt.Print("Container terminated\n")
+			}
+		}()
+
+		return dsn, db_container, nil
+	} else if which_db == "mysql" {
+		db_user := "root"
+		db_password := "root"
+		db_port := os.Getenv("POSTGRES_DB_PORT")
+		db_name := os.Getenv("DB_NAME")
+		db_timezone := os.Getenv("POSTGRES_DB_TIMEZONE")
+
+		db_container, err := mysqlContainer.Run(ctx,
+			"mysql:8.0-bullseye",
+			mysqlContainer.WithDatabase(db_name),
+			mysqlContainer.WithUsername(db_user),
+			mysqlContainer.WithPassword(db_password),
+			testContainers.WithWaitStrategy(
+				wait.ForListeningPort(nat.Port(db_port)).
+					WithStartupTimeout(30*time.Second)),
+		)
+		if err != nil {
+			log.Fatalf("failed to start container: %s", err)
+		}
+
+		fmt.Printf("%v Container started \n", which_db)
+		dsn := fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s", db_user, db_password, db_port, db_name, db_timezone)
+
+		// Clean up the container
+		defer func() {
+			if err := db_container.Terminate(ctx); err != nil {
+				log.Fatalf("failed to terminate container: %s", err)
+			} else {
+				fmt.Print("Container terminated\n")
+			}
+		}()
+
+		return dsn, db_container, nil
+	} else {
+		panic("Invalid database type")
+	}
+}
 
 func connect_to_development_database(which_db string) (db *gorm.DB, err error) {
 	var dsn string
@@ -37,7 +126,7 @@ func connect_to_development_database(which_db string) (db *gorm.DB, err error) {
 }
 
 func connect_to_continuous_integration_database(which_db string) (db *gorm.DB, err error) {
-	dsn, _, _ := utils.SetupTestDB(which_db)
+	dsn, _, _ := SetupTestDB(which_db)
 
 	if which_db == "postgres" {
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -61,10 +150,5 @@ func NewConnection(which_db string) (db *gorm.DB, err error) {
 	} else {
 		return nil, nil
 	}
-
-	// else if env == "cd" {
-	// 	fmt.Println("Connecting to continuous deployment database")
-	// } else if env == "production" {
-	// 	fmt.Println("Connecting to production database")
-	// }
+	// else if env == "cd" {} else if env == "production" {}
 }
