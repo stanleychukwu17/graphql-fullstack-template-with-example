624237d3fafd0bca21a6c1126bd3d1dc6a97bac6
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-09-01 18:47:59 +0100

    refactored for better testing

--- server-golang/controllers/users_controller.go
+++ server-golang/controllers/users_controller.go
@@ -2,20 +2,20 @@ package controllers
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 
-	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/configs"
 	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/models"
+	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/services"
 	"github.com/stanleychukwu17/graphql-fullstack-template-with-example/server-golang/utils"
 )
 
 type UsersController struct {
-	DB *gorm.DB
+	DB           *gorm.DB
+	UserServices services.UserServices
 }
 
 // route to register a new user
@@ -45,31 +45,27 @@ func (u *UsersController) RegisterUser(ctx *fiber.Ctx) error {
 	}
 
 	// check to see if the email already or username exist
-	var found_id uint
-	err := u.DB.Raw("SELECT id FROM users WHERE username = ? OR email = ? LIMIT 1", user.Username, user.Email).Scan(&found_id).Error
-	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(utils.Show_bad_message(err.Error()))
-	}
-	if found_id > 0 {
+	check_usr := u.UserServices.FindUserByUsernameOrEmail(user.Username, user.Email)
+	if len(check_usr.Username) > 0 {
 		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(
 			utils.Show_bad_message("Email or username already exist"),
 		)
 	}
 
 	// hash the password using bcrypt
-	hashedPassword, err := utils.HashPassword(user.Password)
-	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(utils.Show_bad_message(err.Error()))
+	hashedPassword, _ := u.UserServices.HashPassword(user.Password)
+	if len(hashedPassword) > 0 {
+		user.Password = hashedPassword
 	}
-	user.Password = hashedPassword
 
 	// save the new user to the database
 	user.TimeZone = os.Getenv("TIMEZONE")
-	err = u.DB.Create(&user).Error
+	err := u.UserServices.CreateUser(&user)
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.Show_bad_message(err.Error()))
 	}
 
+	// return a success message
 	return ctx.Status(fiber.StatusCreated).JSON(
 		utils.Show_good_message("User created successfully from controller"),
 	)
@@ -77,10 +73,9 @@ func (u *UsersController) RegisterUser(ctx *fiber.Ctx) error {
 
 func (u *UsersController) LoginThisUser(ctx *fiber.Ctx) error {
 	user := models.User{}
-	userDts := models.User{}
-	envs := configs.Envs
+	userDts := &models.User{}
 
-	// Parse the request body & bind it to the book struct
+	// Parse the request body
 	if err := ctx.BodyParser(&user); err != nil {
 		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(
 			utils.Show_bad_message("Invalid request body"),
@@ -100,24 +95,19 @@ func (u *UsersController) LoginThisUser(ctx *fiber.Ctx) error {
 	}
 
 	// checks to see if the user exists in our database
-	err := u.DB.Raw("SELECT id, name, username, password FROM users WHERE username = ? or email = ? LIMIT 1", user.Username, user.Username).Scan(&userDts).Error
-	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(utils.Show_bad_message(err.Error()))
-	}
-
-	// checks to see if any user was found
-	if userDts.ID == 0 {
+	userDts = u.UserServices.FindUserByUsernameOrEmail(user.Username, user.Username)
+	if len(userDts.Username) == 0 {
 		return ctx.Status(fiber.StatusNotFound).JSON(utils.Show_bad_message("User not found"))
 	}
 
 	// compare the password received to see if it is a valid password
-	validPassword := utils.VerifyPassword(userDts.Password, user.Password)
+	validPassword := u.UserServices.VerifyPassword(userDts.Password, user.Password)
 	if !validPassword {
 		return ctx.Status(fiber.StatusUnauthorized).JSON(utils.Show_bad_message("Invalid credentials"))
 	}
 
 	// creates a new session for the user
-	sessionDts := u.createSession(userDts.ID)
+	sessionDts := u.UserServices.CreateSession(userDts.ID)
 
 	// create access and refresh tokens
 	payload := map[string]interface{}{
@@ -125,9 +115,11 @@ func (u *UsersController) LoginThisUser(ctx *fiber.Ctx) error {
 		"created_at":  strings.Split(fmt.Sprintf("%v", sessionDts.CreatedAt), " ")[0],
 	}
 
+	fmt.Printf("payload: %v\n", payload)
+	fmt.Printf("sessionDts: %v\n", sessionDts)
 	// retrieve the accessToken and the refreshToken
-	accessToken, err := utils.SignJWT(payload, envs.JWT_TIME_1)
-	refreshToken, _ := utils.SignJWT(payload, envs.JWT_TIME_2)
+	accessToken, err := utils.SignJWT(payload, os.Getenv("JWT_TIME_1"))
+	refreshToken, _ := utils.SignJWT(payload, os.Getenv("JWT_TIME_2"))
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(utils.Show_bad_message(err.Error()))
 	}
@@ -140,42 +132,4 @@ func (u *UsersController) LoginThisUser(ctx *fiber.Ctx) error {
 		"RefreshToken": refreshToken,
 	}
 	return ctx.Status(fiber.StatusOK).JSON(response)
-}
-
-// createSession creates a new session for the user_id received
-type checkSession struct {
-	Msg string `json:"msg"`
-	models.UsersSession
-}
-
-func (u *UsersController) createSession(userId uint16) checkSession {
-	uSession := checkSession{}
-
-	// checks to see if there are any active sessions for this user
-	u.DB.Raw("SELECT * FROM users_session WHERE user_id = ? and active = 'yes' LIMIT 1", userId).Scan(&uSession)
-	if uSession.ID > 0 && uSession.Active == "yes" {
-		uSession.Msg = "okay"
-		return uSession
-	}
-
-	// create a new session and return it
-	err := u.DB.Raw("INSERT INTO users_session (user_id, fake_id, active, created_at) VALUES (?, ?, 'yes', now())", userId, 0).Scan(&uSession).Error
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	// fetch the current active session
-	u.DB.Raw("SELECT * FROM users_session WHERE user_id = ? and active = 'yes' LIMIT 1", userId).Scan(&uSession)
-	uSession.Msg = "okay"
-	sessionId := uSession.ID
-	new_fake_id := utils.Generate_fake_id(int(sessionId)) // Generate a new fake_id
-
-	// updates the session created with the new fake_id
-	err = u.DB.Raw("UPDATE users_session SET fake_id = ? WHERE id = ?", new_fake_id, sessionId).Scan(&uSession).Error
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	uSession.FakeId = int(new_fake_id)
-	return uSession
 }
