6494ae08a81b412498c84b269b766a91f5d0fd44
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-09-10 02:18:52 +0100

    added utility functions

---
+++ server-golang/test/helper.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+// BeforeEach is a helper function to load the right environment variables for a
+// given test. It loads the .env.test file if the ENV variable is set to "test" or
+// "development", and the .env.ci file if the ENV variable is set to
+// "continuous_integration" or "production". If the ENV variable is not set, it
+// will fail the test.
+func BeforeEach(t *testing.T) error {
+	env, exists := os.LookupEnv("ENV")
+	fmt.Printf("this is the value of env: %v \n", env)
+
+	if exists {
+		if env == "test" || env == "development" {
+			err := godotenv.Load("D:/Sz - projects/0-templates/0-graphql-project-client-and-server/server-golang/.env.test")
+			if err != nil {
+				t.Fatal("Error loading .env file")
+			}
+
+		} else if env == "continuous_integration" || env == "production" {
+			godotenv.Load("D:/Sz - projects/0-templates/0-graphql-project-client-and-server/server-golang/.env.ci")
+
+			_, port_exists := os.LookupEnv("PORT")
+			_, db_exists := os.LookupEnv("DB_NAME")
+
+			if !port_exists || !db_exists {
+				t.Fatal("PORT or DB_NAME is not set, please set your env variables")
+			}
+		} else {
+			t.Logf("ENV is not correct, please set your env variable to either test, continuous_integration or production")
+			return fmt.Errorf("ENV is not correct, please set your env variable to either test, continuous_integration or production")
+		}
+
+		return nil
+	} else {
+		t.Logf("ENV is not set, please set your env variable to either test, continuous_integration or production")
+		return fmt.Errorf("ENV is not set, please set your env variable to either test, continuous_integration or production")
+	}
+}
