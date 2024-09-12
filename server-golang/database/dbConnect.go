adb68a2180287c7bf1c309f9457aa28d4d1546d5
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-09-12 07:43:21 +0100

    removed everything that has to do with timezone

--- server-golang/database/dbConnect.go
+++ server-golang/database/dbConnect.go
@@ -27,7 +27,6 @@ func Setup_continuous_integration_db(which_db string) (string, testcontainers.Co
 		db_password := "password"
 		db_name := os.Getenv("DB_NAME")
 		db_port := os.Getenv("POSTGRES_DB_PORT")
-		db_timezone := os.Getenv("POSTGRES_DB_TIMEZONE")
 
 		// Create container request
 		req := testcontainers.ContainerRequest{
@@ -55,8 +54,7 @@ func Setup_continuous_integration_db(which_db string) (string, testcontainers.Co
 		host, _ := container.Host(ctx)                          // Get container host (IP)
 		port, _ := container.MappedPort(ctx, nat.Port(db_port)) // Get mapped port
 		dsn := fmt.Sprintf(
-			"host=%s user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=%v",
-			host, db_user, db_password, db_name, port.Port(), db_timezone,
+			"host=%s user=%v password=%v dbname=%v port=%v sslmode=disable", host, db_user, db_password, db_name, port.Port(),
 		)
 
 		fmt.Printf("Connecting to postgres DB with DSN: %s\n", dsn)
@@ -65,7 +63,6 @@ func Setup_continuous_integration_db(which_db string) (string, testcontainers.Co
 		db_user := "root"
 		db_password := "root"
 		db_name := os.Getenv("DB_NAME")
-		// db_timezone := os.Getenv("MYSQL_DB_TIMEZONE")
 		db_port := os.Getenv("MYSQL_DB_PORT")
 
 		// Create container request
@@ -92,14 +89,8 @@ func Setup_continuous_integration_db(which_db string) (string, testcontainers.Co
 		host, _ := container.Host(ctx)                          // Get container host (IP)
 		port, _ := container.MappedPort(ctx, nat.Port(db_port)) // Get mapped port
 
-		// old one
-		// dsn := fmt.Sprintf(
-		// 	"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local&loc=%s",
-		// 	db_user, db_password, host, port.Port(), db_name, db_timezone,
-		// )
-
 		dsn := fmt.Sprintf(
-			"%s:%s@tcp(%s:%s)/%s?charset=utf8", db_user, db_password, host, port.Port(), db_name,
+			"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4", db_user, db_password, host, port.Port(), db_name,
 		)
 
 		fmt.Printf("Connecting to mysql DB with DSN: %s\n", dsn)
@@ -132,9 +123,8 @@ func Connect_to_development_database(which_db string) (db *gorm.DB, err error) {
 		DB_PASSWORD := os.Getenv("POSTGRES_DB_PASSWORD")
 		DB_NAME := os.Getenv("DB_NAME")
 		DB_PORT := os.Getenv("POSTGRES_DB_PORT")
-		DB_TIMEZONE := os.Getenv("POSTGRES_DB_TIMEZONE")
 
-		dsn = fmt.Sprintf("host=localhost user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=%v", DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_TIMEZONE)
+		dsn = fmt.Sprintf("host=localhost user=%v password=%v dbname=%v port=%v sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 			Logger: logger.Default.LogMode(logger.Silent), // Only log errors
 		})
@@ -143,9 +133,8 @@ func Connect_to_development_database(which_db string) (db *gorm.DB, err error) {
 		DB_PASSWORD := os.Getenv("MYSQL_DB_PASSWORD")
 		DB_NAME := os.Getenv("DB_NAME")
 		DB_PORT := os.Getenv("MYSQL_DB_PORT")
-		DB_TIMEZONE := os.Getenv("MYSQL_DB_TIMEZONE")
 
-		dsn = fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s", DB_USER, DB_PASSWORD, DB_PORT, DB_NAME, DB_TIMEZONE)
+		dsn = fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?charset=utf8mb4", DB_USER, DB_PASSWORD, DB_PORT, DB_NAME)
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 			Logger: logger.Default.LogMode(logger.Silent), // Only log errors
 		})
@@ -159,7 +148,7 @@ func NewConnection(which_db string) (db *gorm.DB, err error) {
 	fmt.Println("Environment: ", env)
 	fmt.Printf("Connecting to %v database \n", which_db)
 
-	if env == "testing" || env == "development" {
+	if env == "development" {
 		db, err := Connect_to_development_database(which_db)
 		return db, err
 	} else if env == "continuous_integration" {
