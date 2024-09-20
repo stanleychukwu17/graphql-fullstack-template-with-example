c73cc5ca010825d6b9570b60fef317ef13fd5eb9
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-09-20 11:33:34 +0100

    added testing for the middleware

---
+++ server-golang/test/middleware/deserializerUser_test.go
@@ -0,0 +1,22 @@
+package middleware_test
+
+import (
+	"testing"
+)
+
+func TestDeserializerUser(t *testing.T) {
+	// register user and login the user
+	// controllers_test.MockTestRegisterAndLoginUser(t)
+
+	// app, db, err := database.Setup()
+	// if err != nil {
+	// 	t.Fatalf("Could not set up the database and a new Fiber App: %v", err)
+	// }
+
+	// Create a test user
+	// user := controllers_test.rgUserType{
+	// 	User: models.User{
+	// 		Name: "John Doe", Username: "johndoe", Email: "john@example.com", Password: "password", Gender: "male",
+	// 	},
+	// }
+}
