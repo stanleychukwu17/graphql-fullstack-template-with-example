398d67b808e22508ce3a3011f78d73c6017a6f90
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-09-23 10:40:37 +0100

    update: added a test to catch the error when there was no ENV variables for PORT and DB_NAME

--- server-golang/test/helper_test.go
+++ server-golang/test/helper_test.go
@@ -9,25 +9,36 @@ import (
 )
 
 // test to make sure an error is returned when the ENV is not set
-func TestBeforeError(t *testing.T) {
+func TestBeforeEach_Error(t *testing.T) {
 	currentEnv := os.Getenv("ENV")
+	currentBG_USER := os.Getenv("BG_USER")
 
+	// Error when ENV is not set
 	os.Setenv("ENV", "")
 	err := test.BeforeEach(t)
-	require.Error(t, err) // Check that there is an error
+	require.Error(t, err)
 
+	// Error when ENV is not set
 	os.Unsetenv("ENV")
 	err = test.BeforeEach(t)
-	require.Error(t, err) // Check that there is an error
+	require.Error(t, err)
 
+	// Error when ENV  of $PORT & $DB_NAME is not set
+	os.Setenv("ENV", "continuous_integration")
+	os.Setenv("BG_USER", "pass_test")
+	err = test.BeforeEach(t)
+	require.Error(t, err)
+
+	// done with the testing, take things back to the way we met them
 	os.Setenv("ENV", currentEnv)
+	os.Setenv("BG_USER", currentBG_USER)
 }
 
 // test to make sure that when the ENV is set, then the appropriate .env file is loaded
 func TestBeforeEach(t *testing.T) {
 	currentEnv := os.Getenv("ENV")
 
-	os.Setenv("ENV", "test")
+	os.Setenv("ENV", "development")
 	test.BeforeEach(t)
 
 	os.Setenv("ENV", "continuous_integration")
