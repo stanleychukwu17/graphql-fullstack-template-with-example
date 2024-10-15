6cb2574189d2a05823035201d81e7dcd4fd3107e
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-15 10:31:08 +0100

    feat: added page transition loading

--- client/app/components/Header/LoggedOutCard.tsx
+++ client/app/components/Header/LoggedOutCard.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link"
+import { useCallback } from "react"
+
+import { setPageTransition } from "@/app/redux/features/siteSlice"
+import { useAppDispatch } from "@/app/redux/hook"
 
 export default function LoggedOutCard() {
+    const dispatch = useAppDispatch()
+
+    // enable page transition loading
+    const linkClicked = useCallback(() => dispatch(setPageTransition(true)), [])
+
     return (
         <div className="flex space-x-8 font-semibold text-[16.5px]">
             <div className="">
-                <Link href="/login">Register</Link>
+                <Link href="/login" onClick={linkClicked}>Register</Link>
             </div>
             <div className="">
-                <Link href="/login">Login</Link>
+                <Link href="/login" onClick={linkClicked}>Login</Link>
             </div>
         </div>
     )
