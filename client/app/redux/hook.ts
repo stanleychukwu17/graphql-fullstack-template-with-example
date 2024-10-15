0ecd96a7a4a0b2d82d8de6e3af3068acf50c2bdf
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-15 11:12:40 +0100

    redux path update

--- client/app/redux/hook.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./reduxStore";
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppDispatch = () => useDispatch<AppDispatch>()
