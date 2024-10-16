c1849c40056ae340e7790445c274a5a40a25b6fe
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-16 02:10:23 +0100

    update: cleaned up the login page route, users can only login from here, registration has been moved to a new route

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -8,22 +8,13 @@ import { updateUser } from '@/app/utils/redux/features/userSlice';
 import { setPageTransition } from '@/app/utils/redux/features/siteSlice';
 import { BACKEND_PORT as backEndPort } from '@/my.config';
 
-import MessageComp, {MessageCompProps} from "../components/Message/MessageComp";
+import MessageComp, {MessageCompProps} from "@/app/components/Message/MessageComp";
 
 import './page.scss'
 
 type LoginForRHF = {
     username: string
     password: string
-}
-
-type RegisterRHF = {
-    name: string
-    username: string
-    email: string
-    gender: 'male'|'female'
-    password: string
-    confirm_password: string
 }
 
 export default function LoginPage() {
@@ -39,9 +30,7 @@ export default function LoginPage() {
         dispatch(setPageTransition(false))
     }, [])
 
-    // setting up React Hook Form to handle the forms below(i.e both the login and registration forms)
     const { register: registerLogin, handleSubmit: handleLoginSubmit, setValue: loginSetValue, formState: {errors:loginError} } = useForm<LoginForRHF>()
-    const { register: registerReg, handleSubmit: handleRegisterSubmit, setValue: regSetValue, formState: {errors:regError} } = useForm<RegisterRHF>()
 
     const submitLogin: SubmitHandler<LoginForRHF> = async (data) => {
         setIsLoading1(true)
@@ -77,34 +66,11 @@ export default function LoginPage() {
         });
     }
 
-    const submitRegistration: SubmitHandler<RegisterRHF> = (data) => {
-        setIsLoading2(true)
-
-        axios.post(`${backEndPort}/users/registerUser`, data, {headers: {'Content-Type': 'application/json'}})
-        .then((res) => {
-            setShowAlert(true)
-            setAlertMsg({'msg_type':res.data.msg, 'msg_dts':res.data.cause})
-            setIsLoading2(false)
-
-            // clears all of the input field for registering
-            Object.keys(data).forEach((item) => {
-                regSetValue(item as "username" | "password" | "name" | "email" | "gender" | "confirm_password", "")
-            })
-        })
-        .catch((err) => {
-            const msg_dts = `Status: ${err.response.status}, ${err.response.data.cause}` 
-            setShowAlert(true)
-            setAlertMsg({'msg_type':'bad', 'msg_dts':msg_dts})
-            setIsLoading2(false)
-        });
-    }
-
     return (
         <div className="block relative my-14 padding-x">
-            {
-                showAlert &&
+            {showAlert && (
                 <MessageComp {...alertMsg} closeAlert={setShowAlert} />
-            }
+            )}
 
             <div className="pb-10 text-4xl">Hi there!</div>
             <div className="ovrCover flex">
@@ -133,76 +99,6 @@ export default function LoginPage() {
                         <div className="btnCvr">
                             {!isLoading1 && <button type="submit">Login</button>}
                             {isLoading1 && <p>Loading...</p>}
-                        </div>
-                    </form>
-                </div>
-                {/* --END-- */}
-
-                {/* --START-- the registration section */}
-                <div className="w-1/2">
-                    <div className="titleUp">Register</div>
-                    <form onSubmit={handleRegisterSubmit(submitRegistration)}>
-                        <div className="inputCover">
-                            <div className="inpTitle font-bold">
-                                <label htmlFor="name">name</label>
-                            </div>
-                            <div className="inpInput">
-                                <input id="name" type="text" {...registerReg("name", {required: true})} />
-                                {regError.name && <p>This field is required!!!</p>}
-                            </div>
-                        </div>
-                        <div className="inputCover">
-                            <div className="inpTitle font-bold">
-                                <label htmlFor="username">username</label>
-                            </div>
-                            <div className="inpInput">
-                                <input id='username' type="text" {...registerReg("username", {required: true})} />
-                                {regError.username && <p>This field is required!!!</p>}
-                            </div>
-                        </div>
-                        <div className="inputCover">
-                            <div className="inpTitle font-bold">
-                                <label htmlFor="email">email</label>
-                            </div>
-                            <div className="inpInput">
-                                <input id='email' type="text" {...registerReg("email", {required: true})} />
-                                {regError.email && <p>This field is required!!!</p>}
-                            </div>
-                        </div>
-                        <div className="inputCover">
-                            <div className="inpTitle font-bold">
-                                <label htmlFor="gender">gender</label>
-                            </div>
-                            <div className="inpInput">
-                                <select id='gender' {...registerReg("gender", {required: true})}>
-                                    <option value="">Select your gender</option>
-                                    <option value="male">male</option>
-                                    <option value="female">female</option>
-                                </select>
-                                {regError.gender && <p>This field is required!!!</p>}
-                            </div>
-                        </div>
-                        <div className="inputCover">
-                            <div className="inpTitle">
-                                <label htmlFor="password">password</label>
-                            </div>
-                            <div className="inpInput">
-                                <input id="password" type="password" {...registerReg("password", {required: true})} />
-                                {regError.password && <p>This field is required!!!</p>}
-                            </div>
-                        </div>
-                        <div className="inputCover">
-                            <div className="inpTitle">
-                                <label htmlFor="Re-enter Password">Re-enter Password</label>
-                            </div>
-                            <div className="inpInput">
-                                <input id="Re-enter Password" type="password" {...registerReg("confirm_password", {required: true})} />
-                                {regError.confirm_password && <p>This field is required!!!</p>}
-                            </div>
-                        </div>
-                        <div className="btnCvr">
-                            {!isLoading2 && <button type="submit">Register</button>}
-                            {isLoading2 && <p>Loading...</p>}
                         </div>
                     </form>
                 </div>
