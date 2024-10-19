95fd5aa4ce9a651cb119fb9ee1d82e353f01bd8b
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-19 06:32:33 +0100

    now using .env for all my configs

--- client/my.config.ts
+++ client/my.config.ts
@@ -1,4 +1,2 @@
-export const ENV = "development"
-export const BACKEND_PORT = 'http://localhost:4000'
-export const GraphQL_PORT = 'http://localhost:4000/graphql'
-export const CYPRESS_TEST = "DATABASE"
+// export const BACKEND_PORT = 'http://localhost:4000'
+// export const GraphQL_PORT = 'http://localhost:4000/graphql'
