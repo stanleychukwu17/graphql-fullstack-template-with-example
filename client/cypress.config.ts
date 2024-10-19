63232e9b175ae1479f1192565f1a0910f6485522
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-19 13:34:12 +0100

    added config to allow jest and cypress work together

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,5 +1,9 @@
 // const { defineConfig } = require("cypress");
 import { defineConfig } from "cypress";
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config({ path: "./.env" });
 
 module.exports = defineConfig({
   viewportHeight: 1080,
@@ -7,9 +11,17 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-    excludeSpecPattern: ["**/1-getting-started/**", "**/2-advanced-examples/**"],
-    // setupNodeEvents(on, config) {
-    //  implement node event listeners here
-    // },
+    excludeSpecPattern: [
+      "**/1-getting-started/**",
+      "**/2-advanced-examples/**",
+      "**/page-objects/**"
+    ],
+    tsConfig: 'cypress/tsconfig.ts', // Make sure this path is correct
+
+    setupNodeEvents(on, config) {
+      config.env.CYPRESS_TEST_WITH = process.env.CYPRESS_TEST_WITH;
+
+      return config;
+    },
   },
 });
