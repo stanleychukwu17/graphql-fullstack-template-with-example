ead632b421e89f732f5c813dea779ba03be964a5
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-20 09:20:29 +0100

    successful registration test complete

--- client/cypress/e2e/page-objects/auth/register/register_page_object.ts
+++ client/cypress/e2e/page-objects/auth/register/register_page_object.ts
@@ -1,4 +1,5 @@
 import { urlMap } from "../../../../../app/utils/url-mappings/"
+import { generateRandomString } from "../../utils"
 
 type RegisterUserProps = {
     name: string;
@@ -24,38 +25,53 @@ export default class RegisterPageObject {
         cy.get("input#re-enter-password").type(userDts.password)
     }
 
-    async interceptUserRegistrationRequest () {
+    async interceptRegistrationRequest () {
         const cypress_test_with = Cypress.env('CYPRESS_TEST_WITH');
         const requestName = "registerRequest"
+        let mocked = false
 
         if (cypress_test_with === "REAL_DATABASE") {
             // do nothing
         } else if (cypress_test_with === "MOCK_DATABASE") {
-            // Intercept the login request and provide a custom response
-            cy.intercept('POST', urlMap.serverAuth.login, {
+            // Intercept the registration request and provide a custom response
+            mocked = true
+            cy.intercept('POST', urlMap.serverAuth.register, {
                 statusCode: 200,
-                body:{
-                    msg: 'okay',
-                    accessToken: 'mockedToken',
-                    refreshToken: 'mockedRefreshToken',
-                    session_fid: '134535',
-                    name: 'Big stanley'
-                },
+                body: {msg: 'okay', cause: 'registration successful from mocked database'},
             }).as(requestName);
         }
 
-        return {requestName}
+        return {requestName, mocked}
     }
 
     async completeUserRegistration() {
+        const name = `test-${generateRandomString(5)}`
+        const username = `test-${generateRandomString(5)}`
+        const email = `test-${generateRandomString(5)}@testing.com`
+        const password = `stanley`
+        const gender = "male"
+        const fields = {name, username, email, password, gender}
+
+        // intercepts the request, incase we are using the MOCK_DATABASE
+        const {requestName, mocked} = await this.interceptRegistrationRequest()
+
         this.visitRegisterPage()
-        this.fillRegistrationForm({
-            name: "test",
-            username: "test",
-            email: "test",
-            password: "test",
-            gender: "male",
+        this.fillRegistrationForm(fields)
+
+        cy.get("div.btnCvr button[type='submit']").click()
+
+        // if we are using the MOCK_DATABASE, wait for the request to complete
+        if (mocked) cy.wait(`@${requestName}`);
+
+        // assert that the login was successful
+        const loginBtn = cy.get("button[data-testid='btn-msg-comp']")
+        loginBtn.should('be.visible');
+        loginBtn.click().then(() => {
+            // assert that we are on the login page
+            cy.url().should("include", urlMap.clientAuth.login, {timeout: 15000})
+            console.log(cy.url())
         })
-        cy.get("button[type='submit']").click()
+
+        return fields
     }
 }
