f3bbeb47c5658bd737462691a53cfca4bf80f608
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-22 11:53:53 +0100

    added all the test for complete register page e2e testing

--- client/cypress/e2e/specs/auth/register/register_page.spec.ts
+++ client/cypress/e2e/specs/auth/register/register_page.spec.ts
@@ -1,11 +1,73 @@
-import LoginPageObject from "../../../page-objects/auth/login/login_page_object"
-import RegisterPageObject from "../../../page-objects/auth/register/register_page_object"
+import { StatusCodes } from "http-status-codes"
+
+import { urlMap } from "../../../../../app/utils/url-mappings"
+import RegisterPageObject, { userRegistrationDetails } from "../../../page-objects/auth/register/register_page_object"
+import { interceptRequest } from "../../../page-objects/utils"
 
 const registerPage = new RegisterPageObject()
-const loginPageObject = new LoginPageObject()
 
 describe('Register page', () => {
+    let userDts: userRegistrationDetails
+
+    before(() => {
+        userDts = registerPage.generateRegistrationFormFields()
+    })
+
+
     it("should register with valid credentials", () => {
+        // send the registration request
+        const {loginLink} = registerPage.completeUserRegistration(userDts)
+
+        loginLink.click().then(() => {
+            // assert that we are on the login page
+            cy.url({timeout: 10000}).should("include", urlMap.clientAuth.login)
+        })
+    })
+
+    it("should fail to register if the username or email already exists", () => {
+        // Intercept the login request and provide a custom response
+        const requestName = "usernameAlreadyExists"
+        const {mocked} = interceptRequest({
+            requestName,
+            method: "POST",
+            url: urlMap.serverAuth.register,
+            statusCode: StatusCodes.CONFLICT,
+            body: {msg: 'bad', 'cause':'you provided an invalid username or password' }
+        })
+
+        // visit the register page, fill in the registration form, and click the submit button
         registerPage.visitRegisterPage()
+        registerPage.fillRegistrationForm(userDts)
+        registerPage.getSubmitButton().click()
+
+        // if we are using the MOCK_DATABASE, wait for the request to complete
+        if (mocked) cy.wait(`@${requestName}`);
+
+        // assert to be sure that the correct error is displayed
+        cy.contains(StatusCodes.CONFLICT).should("exist")
+    })
+
+    it("should show an error message if there is an issue from the server", () => {
+        // Intercept the login request and provide a custom response
+        const requestName = "failedRegisterRequest"
+        const {mocked} = interceptRequest({
+            requestName,
+            force_mock: true,
+            method: "POST",
+            url: urlMap.serverAuth.register,
+            statusCode: StatusCodes.BAD_REQUEST,
+            body: {msg: 'bad', 'cause':'you provided an invalid username or password' }
+        })
+
+        // visit the register page, fill in the registration form, and click the submit button
+        registerPage.visitRegisterPage()
+        registerPage.fillRegistrationForm(userDts)
+        registerPage.getSubmitButton().click()
+
+        // if we are using the MOCK_DATABASE, wait for the request to complete
+        if (mocked) cy.wait(`@${requestName}`);
+
+        // assert that the registration was not successful
+        cy.contains(StatusCodes.BAD_REQUEST).should("exist")
     })
 })
