b0007d188d38c4ec17b6c41fb9926a81ed6a3e92
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-22 11:50:49 +0100

    update: we added a function now that intercepts the request only if we're are testing with the mock database, otherwise: request is allowed to be sent to the server

--- client/cypress/e2e/page-objects/utils.ts
+++ client/cypress/e2e/page-objects/utils.ts
@@ -9,3 +9,26 @@ export function generateRandomString(length: number): string {
 
     return result;
 }
+
+type interceptProps = {
+    requestName: string;
+    force_mock?: boolean;
+    method: "POST" | "GET";
+    url: string;
+    statusCode: number;
+    body: object;
+}
+export function interceptRequest ({force_mock = false, requestName, method, url, statusCode, body}: interceptProps) {
+    const cypress_test_with = Cypress.env('CYPRESS_TEST_WITH');
+    let mocked = false
+
+    if (cypress_test_with === "MOCK_DATABASE" || force_mock === true) {
+        // Intercept the registration request and provide a custom response
+        mocked = true
+        cy.intercept(method, url, {statusCode, body}).as(requestName);
+    } else if (cypress_test_with === "REAL_DATABASE") {
+        // do nothing
+    }
+
+    return {mocked}
+}
