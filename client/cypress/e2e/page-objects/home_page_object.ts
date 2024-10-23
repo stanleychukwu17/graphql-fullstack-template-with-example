8ecab9e7e8ce35351965f4b8a8b49ee6bdccc215
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-23 07:25:13 +0100

    added home page control class

---
+++ client/cypress/e2e/page-objects/home_page_object.ts
@@ -0,0 +1,19 @@
+import { urlMap } from "../../../app/utils/url-mappings/"
+
+export default class HomePageObject {
+    visitHomePage() {
+        cy.visit(urlMap.home)
+    }
+
+    getLoginLink() {
+        return cy.get("[data-testid='login']")
+    }
+
+    getRegisterLink() {
+        return cy.get("[data-testid='register']")
+    }
+
+    getLogoutLink() {
+        return cy.get("[data-testid='logout']")
+    }
+}
