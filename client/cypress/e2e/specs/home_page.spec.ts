6da25a3acece98d2533c69bb6724e4950889eb98
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-10-23 07:26:40 +0100

    complete all links testing from homepage

--- client/cypress/e2e/specs/home_page.spec.ts
+++ client/cypress/e2e/specs/home_page.spec.ts
@@ -1,6 +1,56 @@
+import { urlMap } from "../../../app/utils/url-mappings/"
+import HomePageObject from "../page-objects/home_page_object"
+import RegisterPageObject, { userRegistrationDetails } from "../page-objects/auth/register/register_page_object"
+import LoginPageObject from "../page-objects/auth/login/login_page_object"
+
+const homePage = new HomePageObject()
+const registerPage = new RegisterPageObject()
+const loginPage = new LoginPageObject()
+
 describe('Home Page', () => {
-    it("should display the home page", () => {
-        cy.visit('/')
-        cy.get('[data-testid="home-page-hero"]').should('be.visible')
+    let userDts: userRegistrationDetails;
+
+    before(() => {
+        userDts = registerPage.generateRegistrationFormFields()
     })
+
+    it("should display the home page and make sure important links from the homePage works", () => {
+        homePage.visitHomePage()
+
+        // first assertions
+        cy.url().should('include', urlMap.home)
+        homePage.getLoginLink().should('be.visible')
+        homePage.getRegisterLink().should('be.visible')
+        homePage.getLogoutLink().should('not.exist')
+
+        // verify that the login link works
+        homePage.getLoginLink().click().then(() => {
+            cy.url().should('include', urlMap.clientAuth.login)
+            cy.go("back")
+        })
+
+        // verify that the register link works
+        homePage.getRegisterLink().click().then(() => {
+            cy.url().should('include', urlMap.clientAuth.register)
+            cy.go("back")
+        })
+    })
+
+    it("should make sure registration, login and logout links are working", () => {
+        const {loginLink} = registerPage.completeUserRegistration(userDts)
+
+        // navigates to the login page and assert that we are on the login page
+        loginLink.click()
+        cy.url({timeout: 10000}).should("include", urlMap.clientAuth.login)
+
+        // log the user in
+        loginPage.completeUserLogin(userDts.username, userDts.password)
+
+        // assert that the logout link is visible
+        // then logout and assert that we are on the home page
+        homePage.getLogoutLink().should('be.visible')
+        homePage.getLogoutLink().click()
+        cy.url({timeout: 10000}).should("include", urlMap.home)
+    })
+
 })
